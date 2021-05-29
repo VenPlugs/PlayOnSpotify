9de4b08d653634cdbf0697f62abd11f8db40090f
Vendicated <vendicated@riseup.net>
2021-05-30 00:59:46 +0200

    Relicense as Apache

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 /* PlayOnSpotify, a powercord plugin to more easily play music on spotify
  * Copyright (C) 2021 Vendicated
  *
- * PlayOnSpotify is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PlayOnSpotify is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PlayOnSpotify.  If not, see <https://www.gnu.org/licenses/>.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
 const { Plugin } = require("powercord/entities");
